08e39de65189e8daebff070d099698d0c5c1bf5a
Sofia <sonykane1999@gmail.com>
2024-03-14 22:20:41 +0300

    delete render file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import state from "./components/redux/state";
 import './index.css';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {rerenderEntireTree} from "./render";
+import {addPost, updateNewPostText} from "./components/redux/state";
+import state from "./components/redux/state";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+let rerenderEntireTree = (state) => {
+    root.render(
+        <React.StrictMode>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+        </React.StrictMode>
+    );
+}
 
 rerenderEntireTree(state);
 
